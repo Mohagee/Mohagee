5eb97a590fb46cce7ab27bd21cde0670e79d0f97
HaON <HaON@HaON>
2020-01-22 16:34:50 +0900

    HaON + BHY

--- Final_MohaGee/src/main/java/com/kh/mohagee/top5/controller/Top5Controller.java
+++ Final_MohaGee/src/main/java/com/kh/mohagee/top5/controller/Top5Controller.java
@@ -1,31 +1,26 @@
 package com.kh.mohagee.top5.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.kh.mohagee.member.model.vo.FavoriteBoard;
 import com.kh.mohagee.top5.model.service.Top5Service;
 
 @Controller
 public class Top5Controller {
 	
-	 private String[] imgExt = {"jpg", "png","PNG", "gif", "bmp", "svg", "jpeg", "webp"};
-	 private String[] videoExt = {"mp4", "avi", "mkv", "wmv", "flv", "asf", "ts", "mpg"};
-	 private String[] audioExt = {"mp3", "ogg", "wav", "flac"};
-
-	 public List<String> imgExtList = Arrays.asList(imgExt);
-	 public List<String> videoExtList = Arrays.asList(videoExt);
-	 public List<String> audioExtList = Arrays.asList(audioExt);
-	 
-	/*
-	 * @Autowired Top5Service top5Service;
-	 */
-	 
+	@Autowired
+	Top5Service top5Service;
 	
-	
-	
-	
+	@RequestMapping("/top5/top5List.do")
+	@ResponseBody
+	public List<FavoriteBoard> top5List(){
+		
+		return top5Service.selectList();
+	}
 
 }
--- Final_MohaGee/src/main/java/com/kh/mohagee/top5/model/dao/To5DAO.java
+++
@@ -1,5 +0,0 @@
-package com.kh.mohagee.top5.model.dao;
-
-public class To5DAO {
-
-}
